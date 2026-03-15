f31459cc52e5599dccfa21762d5e674c5c3b1627
Anandita <73057896+anandita-3217@users.noreply.github.com>
2026-03-15 20:25:16 +0530

    Create customHook.jsx

---
+++ src/customHook.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+// Hook to fetch data from a given URL
+function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+  }, [url]);
+
+  return { data, loading };
+}
